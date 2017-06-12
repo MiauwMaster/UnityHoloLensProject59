a6291cdb748f5ec47358956e57ece9d3912d87e1
MiauwMaster <putm1500@student.nhl.nl>
2017-06-12 13:38:26 +0200

    MoveToPlayer update
    Update movetoplayer vindt gameobject met tag "target". Wanneer enemy player raakt verdwijnt enemy.

--- Assets/Scripts/MoveToPlayer.cs
+++ Assets/Scripts/MoveToPlayer.cs
@@ -9,12 +9,18 @@ public class MoveToPlayer : MonoBehaviour {
 
     void Start()
     {
-        target = Camera.main.transform;
+        target = GameObject.FindGameObjectWithTag("target").transform;
     }
 
     void Update()
     {
         float step = speed * Time.deltaTime;
         transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+
+        if (transform.position == target.position)
+        {
+            //TODO: player lose life
+            Destroy(gameObject);
+        }
     }
 }
