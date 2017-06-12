251f4d77f16de56cdd2c0017a2169810e82d39be
muiz1600 <muiz1600@student.nhl.nl>
2017-06-12 12:49:07 +0200

    Enemy Health System

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    public int health;
+    public bool isDead = false;
+
+    // Use this for initialization
+  
+    void Die ()
+    {
+        isDead = true;
+        Destroy(gameObject);
+    }
+
+    public void LoseLife(int life)
+    {
+        health -= life;
+
+        if (isDead)
+        {
+            return;
+        }
+        
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    
+    }
