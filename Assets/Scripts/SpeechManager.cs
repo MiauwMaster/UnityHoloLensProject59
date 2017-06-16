bfc7d55765cdd515bdf3d2cfe674b4d172a1ddcc
MiauwMaster <putm1500@student.nhl.nl>
2017-06-16 14:34:54 +0200

    voice enabled
    awww yeah

--- Assets/Scripts/SpeechManager.cs
+++ Assets/Scripts/SpeechManager.cs
@@ -8,27 +8,33 @@ public class SpeechManager : MonoBehaviour
 	KeywordRecognizer keywordRecognizer = null;
 	Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
 
-	// Use this for initialization
-	void Start()
+    public GameObject laserTurret;
+    public GameObject gunTurret;
+    public GameObject missileTurret;
+
+    // Use this for initialization
+    void Start()
 	{
-		keywords.Add("Reset world", () =>
+		keywords.Add("Laser", () =>
 		{
-			// Call the OnReset method on every descendant object.
-			this.BroadcastMessage("OnReset");
+            // Call the OnReset method on every descendant object.
+            GetComponent<BuildTurrets>().turret = laserTurret;
 		});
 
-		keywords.Add("Drop Sphere", () =>
-		{
-			var focusObject = GazeGestureManager.Instance.FocusedObject;
-			if (focusObject != null)
-			{
-				// Call the OnDrop method on just the focused object.
-				focusObject.SendMessage("OnDrop");
-			}
-		});
+        keywords.Add("Gun", () =>
+        {
+            // Call the OnReset method on every descendant object.
+            GetComponent<BuildTurrets>().turret = gunTurret;
+        });
 
-		// Tell the KeywordRecognizer about our keywords.
-		keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+        keywords.Add("Missile", () =>
+        {
+            // Call the OnReset method on every descendant object.
+            GetComponent<BuildTurrets>().turret = missileTurret;
+        });
+
+        // Tell the KeywordRecognizer about our keywords.
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
 
 		// Register a callback for the KeywordRecognizer and start recognizing!
 		keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
