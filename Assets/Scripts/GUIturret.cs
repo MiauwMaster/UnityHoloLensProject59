7b9f29d365e4bb5287c3fe855369bc7787c8a7dd
Emiel de Wit <wit1605@student.nhl.nl>
2017-06-22 08:06:06 +0200

    HopelijkWerkendeGUI
    Eindelijk lijkt het te werken. Ik wet het nog niet zeker

---
+++ Assets/Scripts/GUIturret.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIturret : MonoBehaviour {
+
+    [SerializeField]
+    private bool visible;
+    [SerializeField]
+    private bool isActive;
+
+    public bool IsActive { get; internal set;}
+
+
+
+
+    // Use this for initialization
+    void Start () {
+        IsActive = true;
+        if (!visible)
+        {
+            MakeInvisible();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+    public void MakeVisible()
+    {
+        GetComponent<CanvasGroup>().alpha = 1 ;
+        IsActive = true;
+        return;
+    }
+    public void MakeInvisible()
+    {
+        GetComponent<CanvasGroup>().alpha = 0;
+        IsActive = false;
+        return;
+    }
+}
--- Assets/Scripts/SpeechManager.cs
+++ Assets/Scripts/SpeechManager.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Windows.Speech;
 
 public class SpeechManager : MonoBehaviour
@@ -11,29 +12,63 @@ public class SpeechManager : MonoBehaviour
     public GameObject laserTurret;
     public GameObject gunTurret;
     public GameObject missileTurret;
+    [SerializeField]
+    private GUIturret guiCanon;
+    [SerializeField]
+    private GUIturret guiLaser;
+    [SerializeField]
+    private GUIturret guiMissle;
+    [SerializeField]
+    private infoscript info;
+
+ //   public GUIturret guilaser;
 
     // Use this for initialization
     void Start()
 	{
-		keywords.Add("Laser", () =>
-		{       
+        keywords.Add("Laser", () =>
+        {
             GetComponent<BuildTurrets>().turret = laserTurret;
             GetComponent<UpgradeTurrets>().enabled = false;
+            guiLaser.MakeVisible();
+            guiCanon.MakeInvisible();
+            guiMissle.MakeInvisible();
         });
 
         keywords.Add("Cannon", () =>
-        {          
+        {
             GetComponent<BuildTurrets>().turret = gunTurret;
+            guiLaser.MakeInvisible();
+            guiCanon.MakeVisible();
+            guiMissle.MakeInvisible();
         });
 
         keywords.Add("Missile", () =>
-        {           
+        {
             GetComponent<BuildTurrets>().turret = missileTurret;
+            guiLaser.MakeInvisible();
+            guiCanon.MakeInvisible();
+            guiMissle.MakeVisible();
         });
 
         keywords.Add("Upgrade cannons", () =>
         {
             GetComponent<UpgradeTurrets>().UpgradeTurret();
+            guiLaser.MakeInvisible();
+            guiCanon.MakeInvisible();
+            guiMissle.MakeInvisible();
+        });
+
+        keywords.Add("Info", () =>
+        {
+            if (info.Nowvisible)
+            {
+                info.MakeInvisible();
+            }
+            else
+            {
+                info.MakeVisible();
+            }
         });
 
         // Tell the KeywordRecognizer about our keywords.
