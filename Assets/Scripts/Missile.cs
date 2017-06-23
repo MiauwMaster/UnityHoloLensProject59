2aba11e4b2991106d34b2c1dbded5ede58a36bb7
Raikuns <joopwestra@gmail.com>
2017-06-23 13:19:11 +0200

    sound2

--- Assets/Scripts/Missile.cs
+++ Assets/Scripts/Missile.cs
@@ -1,23 +1,61 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 public class Missile : MonoBehaviour {
 
 	#region Variables
-
+	private Transform target;
+	public float damage;
+	public float speed = 70f;
 	#endregion
 
-	
-	#region Unity Methods
-	void Start () 
-	{
-		
-	}
-	
 
-	void Update () 
+	#region Unity Methods
+	/// <summary>
+	/// Set the target
+	/// </summary>
+	/// <param name="_target">Target</param>
+	public void Seek(Transform _target)
 	{
-		
+		target = _target;
 	}
+
+	void Update()
+	{
+		//If target is null, return
+		if (target == null)
+		{
+			Destroy(gameObject);
+			FindObjectOfType<SoundManager>().Play("MissileExplode");
+			return;
+		}
+
+		//set target position
+		Vector3 dir = target.position - transform.position;
+		float distanceThisFrame = speed * Time.deltaTime;
+
+		//If bullet si at the same position as the target
+		if (dir.magnitude <= distanceThisFrame)
+		{
+			//hit the target and return
+			HitTarget();
+			return;
+		}
+
+		//move towards the target
+		transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+
+	}
+
+	/// <summary>
+	/// Deal damage to the target and destroy bullet
+	/// </summary>
+	void HitTarget()
+	{
+		target.GetComponent<Enemy>().LoseLife(damage);
+
+		Destroy(gameObject);
+	}
+
 	#endregion
 }
 
